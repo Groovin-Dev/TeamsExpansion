90894516315311516bc1946ed7bfd459e8853fc9
Groovin <git@groovin.dev>
2024-04-15 18:27:28 -0400

    Another update

--- src/main/kotlin/dev/groovin/teamsexpansion/Plugin.kt
+++ src/main/kotlin/dev/groovin/teamsexpansion/Plugin.kt
@@ -1,34 +1,42 @@
 package dev.groovin.teamsexpansion
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.TextComponent
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.scoreboard.Scoreboard
 import org.bukkit.scoreboard.Team
+
+// I added this because ChatColor is deprecated but this code still works
+const val COLOR_CHAR = '\u00A7'
 
 class TeamsExpansion : PlaceholderExpansion() {
     override fun canRegister(): Boolean = true
     override fun getName(): String = "TeamsExpansion"
     override fun getIdentifier(): String = "team"
     override fun getAuthor(): String = "Groovin-Dev"
-    override fun getVersion(): String = "0.1-SNAPSHOT"
+    override fun getVersion(): String = "0.3-SNAPSHOT"
     override fun getPlaceholders(): List<String> = listOf("color", "prefix", "suffix", "name", "count", "count_<team>").map { "%${identifier}_$it%" }
 
-    override fun onPlaceholderRequest(player: Player, identifier: String): String {
-        val playerTeam = player.name.getPlayerTeam() ?: return ""
+    override fun onPlaceholderRequest(player: Player?, params: String): String {
+        val teamName = params.substringAfter("_")
+        val team = if (params.startsWith("count_")) {
+            scoreboard.getTeam(teamName)
+        } else {
+            player?.let { scoreboard.getEntryTeam(it.name) } ?: scoreboard.getTeam(teamName)
+        }
+
+        if (team == null) {
+            return ""
+        }
 
         return when {
-            "color" in identifier -> playerTeam.getTeamColor()
-            "prefix" in identifier -> playerTeam.getTeamPrefix()
-            "suffix" in identifier -> playerTeam.getTeamSuffix()
-            identifier.startsWith("count_") -> {
-                val teamName = identifier.substringAfter("count_")
-                scoreboard.getTeam(teamName)?.getTeamCount() ?: ""
-            }
-            "count" in identifier -> playerTeam.getTeamCount()
-            "name" in identifier -> playerTeam.name
+            params.startsWith("color") -> team.getTeamColor()
+            params.startsWith("prefix") -> team.getTeamPrefix()
+            params.startsWith("suffix") -> team.getTeamSuffix()
+            params == "count" -> team.getTeamCount()
+            params.startsWith("count_") -> team.getTeamCount()
+            params == "name" -> team.name
             else -> ""
         }
     }
@@ -38,14 +46,10 @@ class TeamsExpansion : PlaceholderExpansion() {
     }
 }
 
-private fun String.getPlayerTeam(): Team? = TeamsExpansion.scoreboard.getEntryTeam(this)
-
 private fun Team.getTeamColor(): String = runCatching { color().toString() }.getOrDefault("")
 
-private fun Team.getTeamPrefix(): String = prefix().getPlainText()
+private fun Team.getTeamPrefix(): String = LegacyComponentSerializer.legacy(COLOR_CHAR).serialize(prefix())
 
-private fun Team.getTeamSuffix(): String = suffix().getPlainText()
+private fun Team.getTeamSuffix(): String = LegacyComponentSerializer.legacy(COLOR_CHAR).serialize(suffix())
 
 private fun Team.getTeamCount(): String = entries.size.toString()
-
-private fun Component.getPlainText(): String = (this as? TextComponent)?.content() ?: ""
