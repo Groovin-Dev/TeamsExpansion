d33df7714f2f98ae8e9694d3e1fe871a36a5b9ac
Groovin-Dev <git@groovin.dev>
2024-10-04 20:20:07 -0400

    Update placeholderapi version

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 
-    compileOnly("me.clip:placeholderapi:2.11.3")
+    compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
 }
 
